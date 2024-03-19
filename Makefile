f70cbbce01e1fabe1eb206069efb91e5facf7afe
igrek51 <igrek51.dev@gmail.com>
2024-03-19 21:29:15 +0100

    update makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
+.PHONY: build run android test web
+
 devices:
 	flutter devices
 
 run:
 	flutter run
+
+run-mobile:
+	flutter run -d 958744f
 
 dart-fix-dry:
 	dart fix --dry-run
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'ToDo Tree',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
