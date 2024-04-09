15d771deb952f6b087e75475d90f88449fc8420b
igrek51 <igrek51.dev@gmail.com>
2024-04-09 21:40:50 +0200

    Adjust title bar icon size

--- lib/views/home/title_bar.dart
+++ lib/views/home/title_bar.dart
@@ -20,78 +20,82 @@ class TitleBar extends StatelessWidget {
     final homeController = Provider.of<HomeController>(context, listen: false);
     final mainMenuRunner = Provider.of<MainMenuRunner>(context, listen: false);
 
-    return Material(
-      elevation: 20,
-      child: Container(
-        height: 60,
-        alignment: Alignment.center,
-        color: theme.colorScheme.primary,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 2.0),
-          child: Row(
-            children: [
-              Visibility(
-                maintainSize: true,
-                maintainAnimation: true,
-                maintainState: true,
-                visible: !browserState.atRoot || homeState.pageView == HomePageView.itemEditor,
-                child: IconButton(
-                  iconSize: 32,
-                  icon: const Icon(
-                    Icons.arrow_back,
-                    size: 28,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {
-                    safeExecute(() {
-                      homeController.goBack();
-                    });
-                  },
-                ),
-              ),
-              IconButton(
-                iconSize: 32,
+    return Container(
+      height: 60,
+      alignment: Alignment.center,
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 2.0),
+        child: Row(
+          children: [
+            Visibility(
+              maintainSize: true,
+              maintainAnimation: true,
+              maintainState: true,
+              visible: !browserState.atRoot || homeState.pageView == HomePageView.itemEditor,
+              child: IconButton(
+                iconSize: 56,
+                padding: EdgeInsets.all(12),
                 icon: const Icon(
-                  Icons.save,
-                  size: 28,
+                  Icons.arrow_back,
+                  size: 32,
                   color: Colors.white,
                 ),
                 onPressed: () {
-                  safeExecute(() async {
-                    await homeController.saveAndExit();
-                  });
-                },
-              ),
-              Expanded(
-                child: _buildTitle(context, browserState),
-              ),
-              PopupMenuButton(
-                iconSize: 32,
-                icon: const Icon(Icons.more_vert, size: 28),
-                itemBuilder: (context) {
-                  return mainMenuRunner.menuActions(context).map((action) {
-                    return PopupMenuItem(
-                      value: action.id,
-                      height: 50,
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      child: Text(
-                        action.name,
-                        style: TextStyle(
-                          fontSize: 17,
-                        ),
-                      ),
-                    );
-                  }).toList();
-                },
-                onSelected: (value) {
-                  final action = mainMenuRunner.menuActions(context).firstWhere((element) => element.id == value);
                   safeExecute(() {
-                    action.action();
+                    homeController.goBack();
                   });
                 },
               ),
-            ],
-          ),
+            ),
+            IconButton(
+              iconSize: 56,
+              padding: EdgeInsets.all(12),
+              icon: const Icon(
+                Icons.save,
+                size: 32,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                safeExecute(() async {
+                  await homeController.saveAndExit();
+                });
+              },
+            ),
+            Expanded(
+              child: _buildTitle(context, browserState),
+            ),
+            PopupMenuButton(
+              iconSize: 52,
+              padding: EdgeInsets.all(10),
+              icon: const Icon(
+                Icons.more_vert,
+                size: 32,
+                color: Colors.white,
+              ),
+              itemBuilder: (context) {
+                return mainMenuRunner.menuActions(context).map((action) {
+                  return PopupMenuItem(
+                    value: action.id,
+                    height: 50,
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      action.name,
+                      style: TextStyle(
+                        fontSize: 17,
+                      ),
+                    ),
+                  );
+                }).toList();
+              },
+              onSelected: (value) {
+                final action = mainMenuRunner.menuActions(context).firstWhere((element) => element.id == value);
+                safeExecute(() {
+                  action.action();
+                });
+              },
+            ),
+          ],
         ),
       ),
     );
